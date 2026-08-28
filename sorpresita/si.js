dc4a8a4f3c13699736f73b4d75f174598a7ac758
Da35544 <dcornejo2306@gmail.com>
2026-08-27 23:30:12 -0500

    Delete sorpresita directory

--- sorpresita/si.js
+++
@@ -1,7 +0,0 @@
-const audioFondo = document.querySelector("#audio-fondo");
-
-audioFondo.volume = 0.25;
-
-audioFondo.play().catch(() => {
-	document.addEventListener("click", () => audioFondo.play(), { once: true });
-});
